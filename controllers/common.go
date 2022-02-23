701a36c6bd6c784b6df454e09c933f1197d6efad
simran2104 <simrandhiman5808@gmail.com>
2022-02-23 10:52:52 +0530

    added functions to the controller

---
+++ controllers/common.go
@@ -0,0 +1,116 @@
+package controller
+
+import (
+	"context"
+
+	"github.com/cloudflare/cfssl/log"
+	examplecomv1 "github.com/simran2104/Wordpress-Operator/api/v1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+func (r *WordpressReconciler) ensureDeployment(request reconcile.Request,
+	instance *examplecomv1.Wordpress,
+	dep *appsv1.Deployment,
+) (*reconcile.Result, error) {
+
+	found := &appsv1.Deployment{}
+
+	err := r.client.Get(context.TODO(), types.NamespacedName{
+		Name:      dep.Name,
+		Namespace: instance.Namespace,
+	}, found)
+
+	if err != nil && errors.IsNotFound(err) {
+
+		// Create the deployment
+		log.Info("Creating a new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+		err = r.client.Create(context.TODO(), dep)
+
+		if err != nil {
+			// Deployment failed
+			log.Error(err, "Failed to create new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+			return &reconcile.Result{}, err
+		}
+		// Deployment was successful
+		return nil, nil
+
+	} else if err != nil {
+		// Error that isn't due to the deployment not existing
+		log.Error(err, "Failed to get Deployment")
+		return &reconcile.Result{}, err
+	}
+
+	return nil, nil
+
+}
+
+func (r *WordpressReconciler) ensureService(request reconcile.Request,
+	instance *examplecomv1.Wordpress,
+	s *corev1.Service,
+) (*reconcile.Result, error) {
+	found := &corev1.Service{}
+	err := r.client.Get(context.TODO(), types.NamespacedName{
+		Name:      s.Name,
+		Namespace: instance.Namespace,
+	}, found)
+	if err != nil && errors.IsNotFound(err) {
+
+		// Create the service
+		log.Info("Creating a new Service", "Service.Namespace", s.Namespace, "Service.Name", s.Name)
+		err = r.client.Create(context.TODO(), s)
+
+		if err != nil {
+			// Creation failed
+			log.Error(err, "Failed to create new Service", "Service.Namespace", s.Namespace, "Service.Name", s.Name)
+			return &reconcile.Result{}, err
+		}
+		// Creation was successful
+		return nil, nil
+
+	} else if err != nil {
+		// Error that isn't due to the service not existing
+		log.Error(err, "Failed to get Service")
+		return &reconcile.Result{}, err
+	}
+
+	return nil, nil
+}
+
+func (r *WordpressReconciler) ensurePVC(request reconcile.Request,
+	instance *examplecomv1.Wordpress,
+	s *corev1.PersistentVolumeClaim,
+) (*reconcile.Result, error) {
+
+	found := &corev1.PersistentVolumeClaim{}
+
+	err := r.client.Get(context.TODO(), types.NamespacedName{
+		Name:      s.Name,
+		Namespace: instance.Namespace,
+	}, found)
+
+	if err != nil && errors.IsNotFound(err) {
+		// Create the PVC
+		log.Info("Creating a new PVC", "PVC.Namespace", s.Namespace, "PVC.Name", s.Name)
+		err = r.client.Create(context.TODO(), s)
+
+		if err != nil {
+			// Creation failed
+			log.Error(err, "Failed to create new PVC", "PVC.Namespace", s.Namespace, "PVC.Name", s.Name)
+			return &reconcile.Result{}, err
+		}
+		// Creation was successful
+		return nil, nil
+
+	} else if err != nil {
+		// Error that isn't due to the pvc not existing
+		log.Error(err, "Failed to get PVC")
+		return &reconcile.Result{}, err
+	}
+
+	return nil, nil
+
+}
--- go.mod
+++ go.mod
@@ -3,8 +3,14 @@ module github.com/simran2104/Wordpress-Operator
 go 1.16
 
 require (
+	github.com/Azure/go-autorest/autorest/adal v0.9.13 // indirect
+	github.com/cloudflare/cfssl v1.6.1 // indirect
+	github.com/cloudflare/go-metrics v0.0.0-20151117154305-6a9aea36fb41 // indirect
 	github.com/onsi/ginkgo v1.16.4
 	github.com/onsi/gomega v1.15.0
+	github.com/zmap/zlint/v2 v2.2.1 // indirect
+	go.uber.org/zap v1.19.0 // indirect
+	k8s.io/api v0.22.1 // indirect
 	k8s.io/apimachinery v0.22.1
 	k8s.io/client-go v0.22.1
 	sigs.k8s.io/controller-runtime v0.10.0
