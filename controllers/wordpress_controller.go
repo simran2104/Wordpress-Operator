8cfc780991f54e6165e8b5e88d4800f67f1aa2ee
simran2104 <simrandhiman5808@gmail.com>
2022-02-23 06:59:45 +0530

    some manifests are added

--- controllers/wordpress_controller.go
+++ controllers/wordpress_controller.go
@@ -1,30 +1,24 @@
-/*
-Copyright 2022.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controllers
 
 import (
 	"context"
+	"fmt"
+	"time"
 
+	"github.com/cloudflare/cfssl/log"
+	examplecomv1 "github.com/simran2104/Wordpress-Operator/api/v1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	examplecomv1 "github.com/simran2104/Wordpress-Operator/api/v1"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
 // WordpressReconciler reconciles a Wordpress object
@@ -32,6 +26,67 @@ type WordpressReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
 }
+
+// Add creates a new Wordpress Controller and adds it to the Manager. The Manager will set fields on the Controller
+// and Start it when the Manager is Started.
+func Add(mgr manager.Manager) error {
+	return add(mgr, newReconciler(mgr))
+}
+
+// newReconciler returns a new reconcile.Reconciler
+func newReconciler(mgr manager.Manager) reconcile.Reconciler {
+	return &WordpressReconciler{Client: mgr.GetClient(), Scheme: mgr.GetScheme()}
+}
+
+// add adds a new Controller to mgr with r as the reconcile.Reconciler
+func add(mgr manager.Manager, r reconcile.Reconciler) error {
+	// Create a new controller
+	c, err := controller.New("wordpress-controller", mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to primary resource Wordpress
+	err = c.Watch(&source.Kind{Type: &examplecomv1.Wordpress{}}, &handler.EnqueueRequestForObject{})
+	if err != nil {
+		return err
+	}
+
+	// TODO(user): Modify this to be the types you create that are owned by the primary resource
+	// Watch for changes to secondary resource Pods and requeue the owner Wordpress
+
+	err = c.Watch(&source.Kind{Type: &appsv1.Deployment{}}, &handler.EnqueueRequestForOwner{
+		IsController: true,
+		OwnerType:    &examplecomv1.Wordpress{},
+	})
+
+	if err != nil {
+		return err
+	}
+
+	err = c.Watch(&source.Kind{Type: &corev1.Service{}}, &handler.EnqueueRequestForOwner{
+		IsController: true,
+		OwnerType:    &examplecomv1.Wordpress{},
+	})
+
+	if err != nil {
+		return err
+	}
+
+	err = c.Watch(&source.Kind{Type: &corev1.PersistentVolumeClaim{}}, &handler.EnqueueRequestForOwner{
+		IsController: true,
+		OwnerType:    &examplecomv1.Wordpress{},
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// blank assignment to verify that ReconcileWordpress implements reconcile.Reconciler
+var _ reconcile.Reconciler = &WordpressReconciler{}
 
 //+kubebuilder:rbac:groups=example.com,resources=wordpresses,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=example.com,resources=wordpresses/status,verbs=get;update;patch
@@ -46,12 +101,77 @@ type WordpressReconciler struct {
 //
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.10.0/pkg/reconcile
-func (r *WordpressReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+
+func (r *WordpressReconciler) Reconcile(ctx context.Context, request ctrl.Request) (ctrl.Result, error) {
+	_ = logf.FromContext(ctx)
 
 	// TODO(user): your logic here
 
-	return ctrl.Result{}, nil
+	reqLogger := logf.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
+	reqLogger.Info("Reconciling Wordpress")
+
+	// Fetch the Wordpress instance
+	//	instance := &examplev1.Wordpress{}
+	wordpress := &examplecomv1.Wordpress{}
+	err := r.Client.Get(context.TODO(), request.NamespacedName, wordpress)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			return reconcile.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return reconcile.Result{}, err
+	}
+
+	var result *reconcile.Result
+
+	// === MYSQL ======
+
+	result, err = r.ensurePVC(request, wordpress, r.pvcForMysql(wordpress))
+	if result != nil {
+		return *result, err
+	}
+
+	result, err = r.ensureDeployment(request, wordpress, r.deploymentForMysql(wordpress))
+	if result != nil {
+		return *result, err
+	}
+	result, err = r.ensureService(request, wordpress, r.serviceForMysql(wordpress))
+	if result != nil {
+		return *result, err
+	}
+
+	mysqlRunning := r.isMysqlUp(wordpress)
+
+	if !mysqlRunning {
+		// If MySQL isn't running yet, requeue the reconcile
+		// to run again after a delay
+		delay := time.Second * time.Duration(5)
+
+		log.Info(fmt.Sprintf("MySQL isn't running, waiting for %s", delay))
+		return reconcile.Result{RequeueAfter: delay}, nil
+	}
+
+	// ===== WORDPRESS =====
+
+	result, err = r.ensurePVC(request, wordpress, r.pvcForWordpress(wordpress))
+	if result != nil {
+		return *result, err
+	}
+
+	result, err = r.ensureDeployment(request, wordpress, r.deploymentForWordpress(wordpress))
+	if result != nil {
+		return *result, err
+	}
+
+	result, err = r.ensureService(request, wordpress, r.serviceForWordpress(wordpress))
+	if result != nil {
+		return *result, err
+	}
+
+	return reconcile.Result{}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
